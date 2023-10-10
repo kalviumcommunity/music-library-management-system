2f79b659990322a98d1f1cc99b5fb693e8ec1c2a
Rishi Ganesh <mr.rishiganesh@gmail.com>
2023-10-10 08:56:32 +0530

    Updated the tables

--- dbqueries.sql
+++ dbqueries.sql
@@ -1,47 +1,47 @@
- create database music_library;
- use music_library;
-  
+CREATE DATABASE music_library;
+USE music_library;
+
 CREATE TABLE users (
     user_id INT PRIMARY KEY,
     user_name VARCHAR(50),
-    password VARCHAR (25) UNIQUE,
-    email VARCHAR (50)
+    password VARCHAR(25) UNIQUE,
+    email VARCHAR(50)
 );
 
 CREATE TABLE artists (
-artist_id INT PRIMARY KEY,
-artist_name VARCHAR (50),
-description VARCHAR (200),
-genre VARCHAR (50)
+    artist_id INT PRIMARY KEY,
+    artist_name VARCHAR(50),
+    description VARCHAR(200),
+    genre VARCHAR(50)
 );
 
 CREATE TABLE albums (
-album_id INT PRIMARY KEY,
-album_title VARCHAR (50),
-artist_id VARCHAR (50),
-release_year INT,
-genre VARCHAR (50)
+    album_id INT PRIMARY KEY,
+    album_title VARCHAR(50),
+    artist_id INT,
+    release_year YEAR,
+    genre VARCHAR(50)
 );
 
 CREATE TABLE songs (
-song_id INT PRIMARY KEY,
-song_title VARCHAR (50),
-album_id VARCHAR (50),
-duration TIMESTAMP,
-song_url VARCHAR (100)
+    song_id INT PRIMARY KEY,
+    song_title VARCHAR(50),
+    album_id INT,
+    duration TIMESTAMP,
+    song_url VARCHAR(100)
 );
 
 CREATE TABLE playlist (
-playlist_id INT PRIMARY KEY,
-user_id INT,
-playlist_title VARCHAR (50),
-description VARCHAR (100)
+    playlist_id INT PRIMARY KEY,
+    user_id INT,
+    playlist_title VARCHAR(50),
+    description VARCHAR(100)
 );
 
 CREATE TABLE playlist_songs (
-playlistsong_id INT PRIMARY KEY,
-playlist_id INT,
-song_id INT
+    playlistsong_id INT PRIMARY KEY,
+    playlist_id INT,
+    song_id INT
 );
 
-show tables;
+SHOW TABLES;
